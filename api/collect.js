d723a6ae8a9181542bc845dee7f9bb7871e86031
RaghavVyas4 <raghavvyas506@gmail.com>
2025-05-14 16:32:24 +0530

    changes

--- api/collect.js
+++ api/collect.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     if (req.method === 'POST') {
         const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         const { answer1, answer2 } = req.body;
