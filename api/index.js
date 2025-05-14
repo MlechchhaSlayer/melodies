71c92d4e6cdcadf400e0de5cf1bcbbeeabba449e
RaghavVyas4 <raghavvyas506@gmail.com>
2025-05-14 06:51:27 +0530

    Updated serverless function for IP logging

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
-// api/index.js
-
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { answer1, answer2 } = req.body;
-        const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-        console.log(`IP: ${ip}, Answer1: ${answer1}, Answer2: ${answer2}`);
-
-        res.status(200).json({ message: 'Data received successfully!' });
+        // Log IP address
+        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        
+        console.log(`Received answers: ${answer1}, ${answer2}`);
+        console.log(`IP Address: ${ip}`);
+        
+        res.status(200).json({ message: 'Data received' });
     } else {
-        res.status(405).json({ message: 'Method Not Allowed' });
+        res.status(405).json({ message: 'Method not allowed' });
     }
 }
