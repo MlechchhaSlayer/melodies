e6ff1fb4fbfdd4d619cb77ed40565f49f1de28c9
RaghavVyas4 <raghavvyas506@gmail.com>
2025-05-14 16:38:15 +0530

    changes

--- api/collect.js
+++ api/collect.js
@@ -8,7 +8,6 @@ export default async function handler(req, res) {
         const { answer1, answer2 } = req.body;
 
         console.log("Received data:", { answer1, answer2, clientIP });
-
         res.status(200).json({ message: 'Data received successfully', answer1, answer2, clientIP });
     } else {
         res.status(405).json({ message: 'Method not allowed' });
