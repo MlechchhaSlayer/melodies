b19560910ad24d162807ec081f50432d585c96a4
RaghavVyas4 <raghavvyas506@gmail.com>
2025-05-14 08:18:17 +0530

    Fix API route and log received data

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
             req.on("data", chunk => {
                 data += chunk;
             });
+
             req.on("end", () => {
                 try {
                     const parsedData = JSON.parse(data);
