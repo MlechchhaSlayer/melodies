6059d6ed3709f375282c604724313bc47cbc992f
MlechchhaSlayer <bslayer143@gmail.com>
2025-05-14 06:40:07 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-// api/index.js
-
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { answer1, answer2 } = req.body;
+        console.log(`Received answers: ${answer1}, ${answer2}`);
+        
+        // Log IP address
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        console.log(`IP Address: ${ip}`);
 
-        console.log(`IP: ${ip}, Answer1: ${answer1}, Answer2: ${answer2}`);
-
-        res.status(200).json({ message: 'Data received successfully!' });
+        res.status(200).json({ message: 'Data received' });
     } else {
-        res.status(405).json({ message: 'Method Not Allowed' });
+        res.status(405).json({ message: 'Method not allowed' });
     }
 }
